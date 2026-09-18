242e95a93c706049bd2082c4b225256e8f5e30e0
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 16:28:45 +0600

    for file

---
+++ .vscode/FORLOOPS.C/forone.c/expone.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Standard for loop (Initialization; Condition; Increment)
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
